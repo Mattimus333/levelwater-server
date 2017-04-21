e120594c461c114ae66390a0b9802a88fa8c6a8a
Sasha Berkowitz <sasha.b510@gmail.com>
2017-04-20 17:08:44 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// refactor to es6
+const express = require('express');
+const dotenv = require('dotenv');
+
+// finish below
+// import login from './routes/something/login';
+
+const app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
+app.get('/', (req, res) => {
+  res.send('drunk with POWER');
+});
+
+
+const PORT = process.env.PORT || 8000;
+
+// const express = require('express');
+app.listen(PORT, () => {
+  if (app.get('env') !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', PORT);
+  }
+});
+
+// below ?
+// export default app;

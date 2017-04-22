b56ce1e29b3e595d6c6c1236b0c83e38df0b1a37
Sasha Berkowitz <sasha.b510@gmail.com>
2017-04-21 19:59:43 -0700

    migrations: sources and users

--- migrations/20170421173633_sources.js
+++ migrations/20170421173633_sources.js
@@ -1,8 +1,19 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('sources', (table) => {
+    table.increments();
+    table.integer('water_systems_id').notNullable().references('id').inTable('water_systems').onDelete('CASCADE');
+    table.string('source_name').notNullable().defaultTo('');
+    table.integer('system_source_id').notNullable().defaultTo(0);
+    table.string('source_type').notNullable().defaultTo('');
+    table.boolean('treatment').notNullable().defaultTo(false);
+    table.integer('capacity').notNullable().defaultTo(0);
+    table.integer('condition').notNullable().defaultTo(0);
+    table.integer('estimated_replacement_cost').notNullable().defaultTo(0);
+    table.integer('estimated_time_to_replacement').notNullable().defaultTo(0);
+    table.integer('status_code').notNullable().defaultTo(0);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('sources');
 };

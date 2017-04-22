3a756a620177c0d9365ff64d8b26c04365eb4973
Sasha Berkowitz <sasha.b510@gmail.com>
2017-04-22 14:54:58 -0700

    migration: algorithm_results

--- migrations/20170421173917_algorithm_results.js
+++ migrations/20170421173917_algorithm_results.js
@@ -1,8 +1,12 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('algorithm_results', (table) => {
+    table.increments();
+    table.integer('water_systems_id').notNullable().references('id').inTable('water_systems').onDelete('CASCADE');
+    table.integer('target_rate').notNullable().defaultTo(0);
+    table.integer('rate_schedule').notNullable().defaultTo(0);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('algorithm_results');
 };

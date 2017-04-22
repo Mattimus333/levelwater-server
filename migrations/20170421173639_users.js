4ebd10a698e53dd349f5a142c7ee31a22ac17371
Matt <mmuhr88@gmail.com>
2017-04-21 18:24:23 -0700

    a little work on migrations

--- migrations/20170421173639_users.js
+++ migrations/20170421173639_users.js
@@ -1,8 +1,10 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('PWS_name').notNullable().defaultTo('');
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+
 };

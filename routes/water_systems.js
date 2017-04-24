6088abfc709fed1cb7fbcff4a424d237e192e7b7
Sasha Berkowitz <sasha.b510@gmail.com>
2017-04-24 13:29:45 -0700

    changed users to ken m

---
+++ routes/water_systems.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex');
+
+const router = express.Router();
--- seeds/users.js
+++ seeds/users.js
@@ -4,9 +4,9 @@ exports.seed = function (knex, Promise) {
       return knex('users').insert([{
         id: 1,
         water_systems_id: 1,
-        first_name: 'Joanne',
-        last_name: 'Rowling',
-        email: 'jkrowling@gmail.com',
+        first_name: 'Ken',
+        last_name: 'M',
+        email: 'KenM@yahooanswers.com',
         hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
         superuser: 'false',
       }]);

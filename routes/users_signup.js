dd43c7a7d253dcaba3d859c4b8867aa6ae979971
Matt <mmuhr88@gmail.com>
2017-04-24 11:48:09 -0700

    got the sign up branch to work fully!

--- routes/users_signup.js
+++ routes/users_signup.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 const usersSignup = (req, res) => {
   const { email, password } = req.body;
-
   if (!email || !email.trim()) {
     return res.send({ status: 400, errorMessage: 'Email must not be blank' });
   }
@@ -27,8 +26,9 @@ const usersSignup = (req, res) => {
         res.send({ status: 400, ErrorMessage: 'Email already exists' });
       } else if (valid.error) {
         res.send({ status: 400, ErrorMessage: 'Improper email format' });
+      } else {
+        return bcrypt.hash(password, 12);
       }
-      return bcrypt.hash(password, 12);
     })
     .then((hashed_password) => {
       const { water_systems_id, first_name, last_name, superuser } = req.body;
@@ -40,28 +40,24 @@ const usersSignup = (req, res) => {
         hashed_password,
         superuser
       };
-
       return knex('users').insert(newUser, '*');
     })
     .then((users) => {
       const user = users[0];
-      const claim = { userId: user.id };
+      const claim = { userId: user };
       const token = jwt.sign(claim, process.env.JWT_KEY, {
         expiresIn: '7 days'
       });
-
       res.cookie('token', token, {
         httpOnly: true,
         expires: new Date(Date.now() + (1000 * 60 * 60 * 24 * 7)),  // 7 days
         secure: router.get('env') === 'production'
       });
-
       delete user.hashed_password;
-
-      res.send(user);
+      res.status(200).json((user));
     })
     .catch((err) => {
-      res.send({ status: 400, ErrorMessage: err });
+      res.sendStatus({ status: 400, ErrorMessage: err });
     });
 }
 

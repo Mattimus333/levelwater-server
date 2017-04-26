ff0d22c375162f7fd40e5cd2a8598d3fce753c86
Sasha Berkowitz <sasha.b510@gmail.com>
2017-04-25 19:58:40 -0700

    refactored migrations, getUser and deleteWaterSystem

--- routes/user_functions/getUser.js
+++ routes/user_functions/getUser.js
@@ -2,19 +2,18 @@ const knex = require('../../knex');
 
 const getUser = (req, res) => {
   if (typeof req.params.userId !== 'number' || req.params.userId === undefined) {
-    res.status(400).set('Content-Type', 'text/plain').send('User ID must be an integer');
-  } else {
-    knex('users')
-    .where('id', req.params.userId)
-    .first()
-    .then((user) => {
-      delete user.hashed_password;
-      res.status(200).json(user);
-    })
-    .catch((err) => {
-      res.status(400).send('user ID is invalid!');
-    });
+    return res.status(400).set('Content-Type', 'text/plain').send('User ID must be an integer');
   }
+  knex('users')
+  .where('id', req.params.userId)
+  .first()
+  .then((user) => {
+    delete user.hashed_password;
+    res.status(200).json(user);
+  })
+  .catch((err) => {
+    res.status(400).send('user ID is invalid!');
+  });
 };
 
 module.exports = getUser;

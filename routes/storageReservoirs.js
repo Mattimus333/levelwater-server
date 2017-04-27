8d40e9ec652f7fc1877f2b153084693016982fb9
Matt <mmuhr88@gmail.com>
2017-04-27 10:06:37 -0700

    started routes for storageReservoirs

--- routes/storageReservoirs.js
+++ routes/storageReservoirs.js
@@ -3,9 +3,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/storageReservoirs/:water_systems_id', authorize, storageReservoirs);
-router.patch('/storageReservoirs/:source_id', authorize, storageReservoirs);
-router.post('/storageReservoirs', authorize, storageReservoirs);
-router.delete('/storageReservoirs/:source_id', authorize, storageReservoirs);
+router.get('/storageReservoirs/:water_systems_id', authorize, getStorageReservoirs);
+router.patch('/storageReservoirs/:source_id', authorize, patchStorageReservoirs);
+router.post('/storageReservoirs', authorize, postStorageReservoirs);
+router.delete('/storageReservoirs/:source_id', authorize, deleteStorageReservoirs);
 
 module.exports = router;

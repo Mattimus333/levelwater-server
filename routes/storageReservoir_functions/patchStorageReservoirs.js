24342b6f4a44acf8ca0906969dabd79aa63b4dc6
Sasha Berkowitz <sasha.b510@gmail.com>
2017-04-28 11:10:52 -0700

    initial fri

---
+++ routes/storageReservoir_functions/patchStorageReservoirs.js
@@ -0,0 +1,30 @@
+const knex = require('../../knex');
+
+const patchSources = (req, res) => {
+  let waterSystemId;
+  knex('users')
+  .where('id', req.claim.userId)
+  .select('water_systems_id')
+  .then((result) => {
+    waterSystemId = result[0].water_systems_id;
+    return knex('storage_resevoirs')
+    .where('id', req.params.source_id)
+    .select('water_systems_id');
+  })
+  .then((sourceResult) => {
+    if (Number(waterSystemId) !== Number(sourceResult[0].water_systems_id)) {
+      return res.send({ status: 400, ErrorMessage: 'source not found!' });
+    }
+    return knex('sources')
+    .where('id', req.params.source_id)
+    .update(req.body);
+  })
+  .then(() => {
+    return res.json(req.body);
+  })
+  .catch((err) => {
+    return res.send({ status: 400, ErrorMessage: err });
+  });
+};
+
+module.exports = patchSources;

49083001d0b6b4840755e5e479d78547ea402dc4
Sasha Berkowitz <sasha.b510@gmail.com>
2017-04-28 13:06:33 -0700

    getRatesFinances

---
+++ routes/ratesFinances_functions/patchRatesFinances.js
@@ -0,0 +1,30 @@
+const knex = require('../../knex');
+
+const patchRatesFinances = (req, res) => {
+  let waterSystemId;
+  knex('users')
+  .where('id', req.claim.userId)
+  .select('water_systems_id')
+  .then((result) => {
+    waterSystemId = result[0].water_systems_id;
+    return knex('storage_resevoirs')
+    .where('id', req.params.ratesFinancesId)
+    .select('water_systems_id');
+  })
+  .then((ratesResult) => {
+    if (Number(waterSystemId) !== Number(ratesResult[0].water_systems_id)) {
+      return res.send({ status: 400, ErrorMessage: 'RatesFinances not found!' });
+    }
+    return knex('storage_resevoirs')
+    .where('id', req.params.ratesFinancesId)
+    .update(req.body);
+  })
+  .then(() => {
+    return res.json(req.body);
+  })
+  .catch((err) => {
+    return res.send({ status: 400, ErrorMessage: err });
+  });
+};
+
+module.exports = patchRatesFinances;

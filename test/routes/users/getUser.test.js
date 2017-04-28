d47003ca93a7af156065b4053a038e5a2161a9e7
Sasha Berkowitz <sasha.b510@gmail.com>
2017-04-27 17:55:09 -0700

    troubleshooting tests

--- test/routes/users/getUser.test.js
+++ test/routes/users/getUser.test.js
@@ -9,36 +9,61 @@ const expect = require('chai').expect;
 
 describe('getUser', () => {
 
+    // before((done) => {
+    //   knex.migrate.latest()
+    //     .then(() => {
+    //       return knex.seed.run()
+    //         .then(() => {
+    //           done();
+    //         }).then(() => {
+    //           // below is hitting server.js with supertest. might need to change app to path of our routes index.
+    //           request(app)
+    //           // below path might be incorrect
+    //             .post('/postUser')
+    //             .set('Accept', 'application/json')
+    //             .set('Content-Type', 'application/json')
+    //             // no below is wrong. need to login with seeded user info
+    //             // maybe not, compare supseasonal and other API's vs bookshelf
+    //             .send({
+    //                "email": "getUserTest@gmail.com",
+    //                "password": "getUserTest",
+    //                "first_name": "Fitzwilliam",
+    //                "last_name": "Darcy",
+    //                "water_systems_id": 1,
+    //                "superuser": "true"
+    //              })
+    //             .end((err, res) => {
+    //               if (err) {
+    //                 return done(err);
+    //               }
+    //
+    //               agent.saveCookies(res);
+    //               done();
+    //             });
+    //         })
+    //         .catch((err) => {
+    //             done(err);
+    //         });
+    //       });
+    // });
     before((done) => {
-      knex.migrate.latest()
-        .then(() => {
-          return knex.seed.run()
-            .then(() => {
-              // below is hitting server.js with supertest. might need to change app to path of our routes index.
-              request(app)
-              // below path might be incorrect
-                .post('/postUser')
-                .set('Accept', 'application/json')
-                .set('Content-Type', 'application/json')
-                // no below is wrong. need to login with seeded user info
-                // maybe not, compare supseasonal and other API's vs bookshelf
-                .send({
-                  email: 'getUser@gmail.com',
-                  password: 'getUserPW'
-                })
-                .end((err, res) => {
-                  if (err) {
-                    return done(err);
-                  }
-
-                  agent.saveCookies(res);
-                  done();
-                });
-            })
-            .catch((err) => {
-                done(err);
-            });
+        knex.migrate.latest()
+          .then(() => {
+            done();
+          })
+          .catch((err) => {
+            done(err);
           });
+      });
+
+    beforeEach((done) => {
+      knex.seed.run()
+        .then(() => {
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
     });
 
     after((done) => {
@@ -89,23 +114,6 @@ describe('getUser', () => {
         }, done)
       })
 
-      it('', done => {
-        request(app)
-        .get('/')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, {
-        }, done)
-      })
-
-      it('', done => {
-        request(app)
-        .get('/')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(404, {'status':404,'ErrorMessage':}, done)
-      })
-
       it('should respond with 404 and Not Found if wrong id', done => {
         request(app)
         .get('/users/3000')
@@ -113,5 +121,6 @@ describe('getUser', () => {
         .expect('Content-Type', /json/)
         .expect({'status':404,'ErrorMessage':'Not Found'}, done)
       })
-  })
+    })
+  });
 });

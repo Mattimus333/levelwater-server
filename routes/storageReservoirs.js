7891a28324d015a21b151a1368a8e52478a7cf24
Matt <mmuhr88@gmail.com>
2017-04-27 17:55:54 -0700

    commented out the unfinished patch route

--- routes/storageReservoirs.js
+++ routes/storageReservoirs.js
@@ -7,7 +7,7 @@ const postStorageReservoirs = require('./storageReservoir_functions/postStorageR
 const router = express.Router();
 
 router.get('/storageReservoirs/:water_systems_id', authorize, getStorageReservoirs);
-router.patch('/storageReservoirs/:source_id', authorize, patchStorageReservoirs);
+// router.patch('/storageReservoirs/:source_id', authorize, patchStorageReservoirs);
 router.post('/storageReservoirs', authorize, postStorageReservoirs);
 router.delete('/storageReservoirs/:source_id', authorize, deleteStorageReservoirs);
 

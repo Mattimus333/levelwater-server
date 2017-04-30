110e7c7ead3b2577d7821ff8ab5ff12bdaa6e8d4
Ryan Thissen <rmt1855@gmail.com>
2017-04-30 13:34:59 -0700

    created treatment functions folder, getTreatment route and helper function

---
+++ routes/treatment.js
@@ -0,0 +1,5 @@
+const authorize = require('./authorize');
+const express = require('express');
+const getTreatment = require('../treatment_functions/getTreatment');
+
+const router = express.Router();

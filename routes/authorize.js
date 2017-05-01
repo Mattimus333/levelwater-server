d2a717b2949aa5e8f7d18bbfe66523afb1f852c4
Matt <mmuhr88@gmail.com>
2017-05-01 11:16:03 -0700

    changed the auth function so it looks for token in the header instead of in a cookie

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 const authorize = (req, res, next) => {
-  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+  console.log(req.headers.token);
+  jwt.verify(req.headers.token, process.env.JWT_KEY, (err, payload) => {
     if (err) {
       return res.send({ status: 401, errorMessage: 'Unauthorized' });
     }

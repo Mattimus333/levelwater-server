5525465afd0e213ddc8621aa4a3f4102aab7dae4
Sasha Berkowitz <sasha.b510@gmail.com>
2017-05-01 13:19:56 -0700

    added error messages

--- routes/ratesFinances_functions/postRatesFinances.js
+++ routes/ratesFinances_functions/postRatesFinances.js
@@ -31,31 +31,31 @@ const postRatesFinances = (req, res) => {
     return res.status(400).send('Water systems id must not be blank');
   }
   if (typeof current_average_water_rate !== 'number') {
-    return res.status(400).send('2');
+    return res.status(400).send('Current average water rate must be a number');
   }
   if (typeof total_financial_reserves !== 'number') {
-    return res.status(400).send('3');
+    return res.status(400).send('must be a number');
   }
   if (typeof annual_revenue_water_sales !== 'number') {
-    return res.status(400).send('4');
+    return res.status(400).send('Annual revenue water sales must be a number');
   }
   if (typeof annual_revenue_fees_charged !== 'number') {
-    return res.status(400).send('5');
+    return res.status(400).send('Annual revenue fees charged must be a number');
   }
   if (typeof annual_revenue_subsidies !== 'number') {
-    return res.status(400).send('6');
+    return res.status(400).send('Annual revenue subsidies must be a number');
   }
   if (typeof annual_savings_to_financial_reserves !== 'number') {
-    return res.status(400).send('7');
+    return res.status(400).send('Annual savings to financial reserves must be a number');
   }
   if (typeof annual_personnel_costs !== 'number') {
-    return res.status(400).send('8');
+    return res.status(400).send('Annual personnel costs must be a number');
   }
   if (typeof annual_operations_costs !== 'number') {
-    return res.status(400).send('9');
+    return res.status(400).send('Annual oeprations costs must be a number');
   }
   if (typeof annual_debt_costs !== 'number') {
-    return res.status(400).send('10');
+    return res.status(400).send('Annual debt costs must be a number');
   }
 
   knex('users')

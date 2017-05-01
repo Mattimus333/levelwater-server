2b9dd8b8724114a9c45d7f407f8257c21b3d30cd
Matt <mmuhr88@gmail.com>
2017-05-01 11:39:09 -0700

    getting rid of console log cause auth works!

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const authorize = (req, res, next) => {
-  console.log(req.headers.token);
   jwt.verify(req.headers.token, process.env.JWT_KEY, (err, payload) => {
     if (err) {
       return res.send({ status: 401, errorMessage: 'Unauthorized' });

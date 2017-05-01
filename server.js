06409a11b9b818af280554d50c93228c76f7be5a
Sasha Berkowitz <sasha.b510@gmail.com>
2017-05-01 11:51:44 -0700

    cors

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ const app = express();
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
+
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cookieParser());

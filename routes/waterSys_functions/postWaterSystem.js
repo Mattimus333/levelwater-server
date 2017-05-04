49cf22e366ff535c8b755f6dbb6cad9f23392de5
Matt <mmuhr88@gmail.com>
2017-05-03 18:17:54 -0700

    added a post test for the watersystems and changed the postRoute so that it returns the whole water system, not just the ID

--- routes/waterSys_functions/postWaterSystem.js
+++ routes/waterSys_functions/postWaterSystem.js
@@ -3,31 +3,30 @@ const knex = require('../../knex');
 const postWaterSystems = (req, res) => {
   const { pws_name, pws_id, population, connections } = req.body;
   if (!pws_name || !pws_name.trim()) {
-    return res.status(400).send('Pws_name must not be blank');
+    return res.send({ status: 400, ErrorMessage: 'PWS name must not be blank' });
   }
   if (!pws_id) {
-    return res.status(400).send('Pws_id must not be blank');
+    return res.send({ status: 400, ErrorMessage: 'PWS id must not be blank' });
   }
   if (!population) {
-    return res.status(400).send('Population must not be blank');
+    return res.send({ status: 400, ErrorMessage: 'Population must not be blank' });
   }
   if (!connections) {
-    return res.status(400).send('Connections must not be blank');
+    return res.send({ status: 400, ErrorMessage: 'Connections must not be blank' });
   }
 
   const waterSystem = { pws_name, pws_id, population, connections };
-  let waterSystemId;
 
   knex('water_systems')
   .insert(waterSystem)
   .then((result) => {
-    waterSystemId = result[0];
+    waterSystem.id = result[0];
     return knex('users')
     .where('id', req.claim.userId)
     .update('water_systems_id', result[0]);
   })
   .then(() => {
-    res.status(200).json(waterSystemId);
+    res.status(200).json(waterSystem);
   })
   .catch((err) => {
     res.send({ status: 400, ErrorMessage: err });
